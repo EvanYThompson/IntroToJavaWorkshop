f25a6e460b523daf3112d94ebd0e1503b55c3b20
EvanYThompson <league@WorthyMini.attlocal.net>
2017-01-17 17:00:54 -0800

    day 2 workshop

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,26 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
+		int w= 500;
+		int f= 90;
+		int k= 400;
 		
+Robot stephano=new Robot () ;
+	stephano.penDown();
+	stephano.sparkle();
+	stephano.setSpeed(10);
+for(int i=0;i<4;i++){
+    
+	stephano.move(w);
+	stephano.turn(f);
+}
+	stephano.hide();
+	stephano.penUp();
+	stephano.move(k);
+	stephano.turn(f);
+	
+	
+
 		
 	}
 }
